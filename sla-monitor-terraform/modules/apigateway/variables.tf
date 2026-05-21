aae2f7beba8ba913712e3ce9bf55621c84299e2f
Walid <walid.idrissi.labs@gmail.com>
2026-05-21 15:34:17 +0100

    fix (apigw variables) : replace qualified_arn with invoke_arn for Lambda integrations

--- sla-monitor-terraform/modules/apigateway/variables.tf
+++ sla-monitor-terraform/modules/apigateway/variables.tf
@@ -10,10 +10,26 @@ variable "cognito_app_client_id" {
   type        = string
 }
 
+variable "api_lambda_invoke_arn" {
+  type        = string
+  description = "Invoke ARN for the API Lambda integration"
+}
+
+variable "project_manager_lambda_invoke_arn" {
+  type        = string
+  description = "Invoke ARN for the Project Manager Lambda integration"
+}
+
 variable "api_lambda_qualified_arn" {
   type        = string
+  default     = null
+  #!integration_uri for AWS_PROXY must be the Lambda's invoke_arn
+  description = "Deprecated. Use api_lambda_invoke_arn instead"
 }
 
 variable "project_manager_lambda_qualified_arn" {
   type        = string
+  default     = null
+  #!integration_uri for AWS_PROXY must be the Lambda's invoke_arn
+  description = "Deprecated. Use project_manager_lambda_invoke_arn instead"
 }
