403f87ececfccf166b2de152aa4f3f023471fb7d
Walid <walid.idrissi.labs@gmail.com>
2026-05-25 02:22:12 +0100

    fix(frontend build error)

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import TopNav from '../components/TopNav'
 import ProjectCard from '../components/ProjectCard'
 import AddProjectModal from '../components/AddProjectModal'
